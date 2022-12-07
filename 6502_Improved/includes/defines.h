447f00dfeb5656280d49753215201b65ca1692ce
JoenTheWizard <75371093+JoenTheWizard@users.noreply.github.com>
2022-12-07 04:49:25 +0000

    Add files via upload

---
+++ 6502_Improved/includes/defines.h
@@ -0,0 +1,17 @@
+//Init assembly memory
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#define ASM_MEMORY 4096
+
+//Init ROM
+#define ROM 0x07FF
+
+//Define console clear ANSI escape codes
+#define CNSCLR() printf("\033[H\033[J")
+
+//Break function
+//#define BREAK
